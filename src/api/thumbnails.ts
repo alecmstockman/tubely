b3a90ad93a697b4749868aa6b34089d0fb450840
alecmstockman <alecmstockman@gmail.com>
2026-04-29 14:27:48 -0500

    handlerUploadThumbnail now says Base64 thumbnail to db

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,10 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -43,11 +13,38 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   }
 
   const token = getBearerToken(req.headers);
-  const userID = validateJWT(token, cfg.jwtSecret);
+  const userId = validateJWT(token, cfg.jwtSecret);
+  
+  console.log("uploading thumbnail for video", videoId, "by user", userId);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  const data = await req.formData();
+  const thumbnail = data.get("thumbnail");
 
-  // TODO: implement the upload here
+  if ( ! (thumbnail instanceof File)) {
+    throw new BadRequestError("thumbnail is not a File");
+  }
 
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 10 * 1024 * 1024;
+  const mediaType = thumbnail.type;
+
+  const arrayBuffer = await thumbnail.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  const bufferBase64 = buffer.toString("base64");
+
+  const video = getVideo(cfg.db, videoId);
+
+  if (video?.userID !== userId) {
+    throw new UserForbiddenError("Access to video denied");
+  }
+
+  if (!video) {
+    throw new BadRequestError("Unable to retrieve video")
+  }
+
+  const dataURL = `data:${mediaType};base64,${bufferBase64}`;
+
+  video.thumbnailURL = dataURL;
+  updateVideo(cfg.db, video);
+ 
+  return respondWithJSON(200, video);
 }
